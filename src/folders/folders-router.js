f2ce3c8cd5ced03fcf77d59dae2c3a67d64d0cf8
Sacha Sedriks <sachased@gmail.com>
2020-07-28 21:28:25 -0700

    added /folder/:folder_id enpoint to router for GET

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -39,4 +39,28 @@ foldersRouter
             .catch(next)
     })
 
+    foldersRouter
+        .route('/:folder_id')
+        .all((req, res, next) => {
+            FoldersService.getById(
+                req.app.get('db'),
+                req.params.folder_id
+            )
+                .then(folder => {
+                    if (!folder) {
+                        return res.status(404).json({
+                            error: { message: `Folder doesn't exist`}
+                        })
+                    }
+                    res.folder = folder
+                    next()
+                })
+                .catch(next)
+        })
+        .get((req, res, next) => {
+            res.json(res.folder)
+        })
+    
+
+
 module.exports = foldersRouter;
